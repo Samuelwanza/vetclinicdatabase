9fdd51bf9f0fcb3558f8d81c261b54de5c519c6a
Samuel wanza <s.wanza@alustudent.com>
2023-09-02 14:43:21 +0200

    updated queries.sql

--- queries.sql
+++ queries.sql
@@ -55,7 +55,7 @@ SELECT animals.name AS animal_name,vets.name AS vet_name,visits.date_of_visit FR
 SELECT animals.name AS animal_name,COUNT(animals.name) AS number_visits,vets.name AS vet_name from animals JOIN visits ON animals.id=visits.animals_id JOIN vets ON visits.vet_id=vets.id WHERE vets.name='Stephanie Mendez' GROUP BY animals.name,vets.name;
 SELECT * FROM vets LEFT JOIN specializations ON vets.id=specializations.vet_id LEFT JOIN species ON specializations.species_id=species.id;
 SELECT animals.name AS animal_name,vets.name AS vet_name, visits.date_of_visit from animals JOIN visits ON visits.animals_id=animals.id JOIN vets ON visits.vet_id=vets.id WHERE visits.date_of_visit BETWEEN '2020-04-01' AND '2020-08-30' AND vets.name='Stephanie Mendez';
-SELECT animals.name AS animal_name,COUNT(animals.name) AS number_visits from animals JOIN visits ON animals.id=visits.animals_id GROUP BY animals.name;
+SELECT animals.name AS animal_name,COUNT(animals.name) AS number_visits from animals JOIN visits ON animals.id=visits.animals_id GROUP BY animals.name ORDER BY number_visits DESC LIMIT 1;
 SELECT animals.name AS animal_name,visits.date_of_visit FROM animals JOIN visits ON animals.id=visits.animals_id JOIN vets ON visits.vet_id=vets.id WHERE vets.name='Maisy Smith' ORDER BY visits.date_of_visit ASC LIMIT 1;
 SELECT * FROM animals JOIN visits ON animals.id=visits.animals_id JOIN vets ON visits.vet_id=vets.id ORDER BY visits.date_of_visit DESC LIMIT 1;
 SELECT COUNT(animals.name) As number_visits FROM animals FULL JOIN visits ON animals.id=visits.animals_id FULL JOIN vets ON visits.vet_id=vets.id FULL JOIN specializations ON vets.id=specializations.vet_id FULL JOIN species ON specializations.species_id=species.id WHERE specializations.species_id IS NULL;
